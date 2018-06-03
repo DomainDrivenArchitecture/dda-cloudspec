e8f7d0c7e4e625207ff9f485f4e6658e6e918b1f
jem <michael.jerger@meissa-gmbh.de>
2018-06-03 16:53:19 +0200

    serverspec now is working

--- main/terraform/instance.tf
+++ main/terraform/instance.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "instance" {
-  ami           = "ami-82cf0aed"
+  ami           = "${data.aws_ami.ubuntu_16.id}"
   instance_type = "t2.micro"
+  key_name      = "${aws_key_pair.jem.key_name}"
 }
