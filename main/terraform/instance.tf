668449d6443f1956b77f24cebff90124aa090c05
jem <michael.jerger@meissa-gmbh.de>
2018-06-05 08:28:37 +0200

    get local public ssh key from users home

--- main/terraform/instance.tf
+++ main/terraform/instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
   ami           = "${data.aws_ami.ubuntu_16.id}"
   instance_type = "t2.micro"
-  key_name      = "${aws_key_pair.jem.key_name}"
+  key_name      = "${aws_key_pair.developer_ssh_key.key_name}"
 }
--- main/terraform/key_pair.tf
+++ main/terraform/key_pair.tf
@@ -1,4 +1,4 @@
-resource "aws_key_pair" "jem" {
-  key_name   = "jem"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCeO+eiYDonq3OfxyaUx259y/1OqbhLciD4UlCkguD5PgOuXw+kCXS1Wbdor9cvU8HnsL2j70sPSwCWkcDrrGQ0kpC0GuNO47pKawAOSv07ELpSIIp/nPK5AX2+qI1H3MADBWBE5N1L7sdgatON2A/cC3u5pzcWDaEH7/IJdOkRm8H+qqG+uva6ceFUoYFiJKDixmsmaUXhhDcfYhfpAPBUCSes+HTeT/hk6pdLTX9xXd4H5wyAc+j1e6kPq9ZcxvzZNr9qEMIFjnNL/S9w1ozxQa3sKJQHj8SyVZDlwjvepGS7fKrdlRps938A7I3Y4BaXGX//M1y2HNbUWbMOllLL jem@meissa-ide"
+resource "aws_key_pair" "developer_ssh_key" {
+  key_name   = "cloudspec_ssh_key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
