f87c65a5eed682a0135a0ab5749acec9b755f49c
jem <michael.jerger@meissa-gmbh.de>
2018-06-18 15:18:13 +0200

    server to be created

--- main/hetzner-tf/main.tf
+++ main/hetzner-tf/main.tf
@@ -6,3 +6,19 @@ variable "api_token" {}
 provider "hcloud" {
   token = "${var.api_token}"
 }
+
+resource "hcloud_ssh_key" "developer_ssh_key" {
+  name       = "cloudspec_ssh_key"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "hcloud_server" "instance" {
+  name        = "cloudspec-test"
+  image       = "ubuntu-18.04"
+  server_type = "cx11"
+  ssh_keys    = ["${hcloud_ssh_key.developer_ssh_key.id}"]
+}
+
+output "instance_public_ip" {
+  value = "${hcloud_server.instance.ipv4_address}"
+}
